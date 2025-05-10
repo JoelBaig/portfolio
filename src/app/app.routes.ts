22708ca74bd6508bba5f860f4f2e15823c931617
JoelBaig <131587984+Joe482@users.noreply.github.com>
2025-05-10 23:11:16 +0200

    added components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LandingPageComponent },
+];
---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [HeaderComponent, NavbarComponent],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss'
+})
+export class LandingPageComponent {
+constructor() {
+  console.log('landingPage loaded');
+}
+}
