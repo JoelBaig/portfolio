805b0884a7f7a38aabbffc5efd87b20db5788ecc
JoelBaig <131587984+Joe482@users.noreply.github.com>
2025-05-16 13:39:43 +0200

    added sliding dot to language select

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
 
+export class NavbarComponent {
+  selectedLanguage: 'en' | 'de' = 'en';
+  dotAnimationClass = '';
+
+  selectLanguage(lang: 'en' | 'de') {
+    if (lang === this.selectedLanguage) return;
+
+    this.dotAnimationClass =
+      lang === 'de' ? 'dot-animate-right' : 'dot-animate-left';
+
+    this.selectedLanguage = lang;
+  }
 }
