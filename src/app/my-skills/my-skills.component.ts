4a7824df9f51bfac5a9e3a8e0e084a0c97b073c3
JoelBaig <131587984+Joe482@users.noreply.github.com>
2025-07-07 16:22:20 +0200

    finished pull to peel sticker

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -1,11 +1,45 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-skills',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
-export class MySkillsComponent {
 
+export class MySkillsComponent {
+  stickers = [
+    'assets/img/header/skills/sticker.png',
+    'assets/img/header/skills/sticker_hover.png',
+    'assets/img/header/skills/sticker_open.png'
+  ];
+
+  currentSticker = this.stickers[0];
+  isOpen = false;
+  private isAnimating = false;
+
+  animateSticker() {
+    if (this.isAnimating) return;
+
+    if (this.isOpen) {
+      this.currentSticker = this.stickers[0];
+      this.isOpen = false;
+      return;
+    }
+
+    this.isAnimating = true;
+    this.currentSticker = this.stickers[1];
+
+    setTimeout(() => {
+      this.currentSticker = this.stickers[2];
+      this.isAnimating = false;
+      this.isOpen = true;
+    }, 60);
+  }
+
+  resetSticker() {
+    this.currentSticker = this.stickers[0];
+    this.isOpen = false;
+  }
 }
