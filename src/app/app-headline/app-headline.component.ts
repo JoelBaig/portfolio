eb19e7c5ea7a2bee6e55c4e72ebadb6f551aba7a
JoelBaig <131587984+Joe482@users.noreply.github.com>
2025-08-09 18:32:28 +0200

    edited projects responsive

--- src/app/app-headline/app-headline.component.ts
+++ src/app/app-headline/app-headline.component.ts
@@ -12,16 +12,6 @@ import { Component, Input } from '@angular/core';
   styleUrl: './app-headline.component.scss'
 })
 export class AppHeadlineComponent {
-  // @Input() title: string = 'Section Title';
-  // @Input() subtitle: string = '';
-  // @Input() infoText: string = '';
-  // @Input() lineImage: string = '';
-  // @Input() animateLine: boolean = false;
-  // @Input() lineWidth: string = '340px';
-  // @Input() maskDirection: 'horizontal' | 'circular' = 'horizontal';
-  // @Input() textAlign: 'left' | 'right' | 'center' = 'left';
-  // @Input() animationDirection: 'ltr' | 'rtl' = 'ltr';
-
   @Input() title: string = 'Section Title';
   @Input() subtitle: string = '';
   @Input() infoText: string = '';
