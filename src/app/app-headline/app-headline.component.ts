4a8ddc37d8ad129ea8a0086822c6a37c59f28a4a
JoelBaig <131587984+Joe482@users.noreply.github.com>
2025-10-05 15:22:07 +0200

    edited colleagues thoughts animated line

--- src/app/app-headline/app-headline.component.ts
+++ src/app/app-headline/app-headline.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule, NgClass } from '@angular/common';
+import { CommonModule, NgClass} from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
@@ -22,14 +22,14 @@ export class AppHeadlineComponent {
   @Input() subtitleTag: 'h1' | 'h2' | 'h3' = 'h3';
   @Input() infoOffset: number = 16;
   @Input() infoText: string = '';
-  @Input() lineImage: string = '';
   @Input() animateLine: boolean = false;
+  @Input() lineImage: string = '';
   @Input() lineWidth: number = 40;
   @Input() lineMarginBottom: number = 50;
-  @Input() lineAlign: 'flex-start' | 'center' | 'flex-end' = 'center';
-  @Input() maskDirection: 'horizontal' | 'rtl' | 'circular' = 'horizontal';
-  @Input() textAlign: 'left' | 'right' | 'center' = 'left';
   @Input() lineAboveTitle: boolean = false;
   @Input() lineLeftOfSubtitle: boolean = false;
   @Input() lineUnderInfoText: boolean = false;
+  @Input() lineAlign: 'flex-start' | 'center' | 'flex-end' | 'space-between' | 'space-around' = 'center';
+  @Input() maskDirection: 'horizontal' | 'rtl' | 'circular' = 'horizontal';
+  @Input() textAlign: 'left' | 'right' | 'center' = 'left';
 }
