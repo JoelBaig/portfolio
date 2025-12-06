b5586ae7d51f10ac19fdf4b7078bac3450b1ba02
JoelBaig <131587984+Joe482@users.noreply.github.com>
2025-12-06 12:30:07 +0100

    refactor: added legal notice component to footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LegalNoticeComponent } from '../../legal-notice/legal-notice.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
+    LegalNoticeComponent
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
