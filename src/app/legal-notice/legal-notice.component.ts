bb2cdba8c696eba8b9375336fc9372886b0846ed
JoelBaig <131587984+Joe482@users.noreply.github.com>
2025-12-15 19:45:48 +0100

    refactor: changed legal notice txt

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,7 +1,6 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-legal-notice',
@@ -9,7 +8,6 @@ import { FooterComponent } from '../shared/footer/footer.component';
   imports: [
     CommonModule,
     NavbarComponent,
-    FooterComponent
   ],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
