d471fdb4aebb5aaba85d5ce2925bac6439eba7d0
JoelBaig <131587984+Joe482@users.noreply.github.com>
2026-03-28 09:29:38 +0100

    feat: whole page responsive

--- src/app/colleagues-thoughts/colleagues-thoughts.component.ts
+++ src/app/colleagues-thoughts/colleagues-thoughts.component.ts
@@ -1,155 +1,3 @@
-// import { Component, HostListener } from '@angular/core';
-// import { AppHeadlineComponent } from '../app-headline/app-headline.component';
-// import { CommonModule } from '@angular/common';
-// import { TranslateModule } from '@ngx-translate/core';
-
-// @Component({
-//   selector: 'app-colleagues-thoughts',
-//   imports: [
-//     AppHeadlineComponent,
-//     CommonModule,
-//     TranslateModule
-//   ],
-//   templateUrl: './colleagues-thoughts.component.html',
-//   styleUrl: './colleagues-thoughts.component.scss'
-// })
-// export class ColleaguesThoughtsComponent {
-//   imageCards = [
-//     {
-//       src: 'assets/img/header/colleagues_thoughts/feedback1.png',
-//       srcMobile: 'assets/mobile/mobile_card_1.png',
-//       hoverOverlay: 'assets/img/header/colleagues_thoughts/feedback_hover.png',
-//       icon: 'assets/img/header/colleagues_thoughts/linkedin.png',
-//       iconHover: 'assets/img/header/colleagues_thoughts/linkedin_hover.png',
-//       text: 'COLLEAGUES.CARD_TXT_1',
-//       name: 'Lukas Brenner',
-//       job: 'Frontend Developer'
-//     },
-//     {
-//       src: 'assets/img/header/colleagues_thoughts/feedback2.png',
-//       srcMobile: 'assets/mobile/mobile_card_2.png',
-//       hoverOverlay: 'assets/img/header/colleagues_thoughts/feedback_hover.png',
-//       icon: 'assets/img/header/colleagues_thoughts/linkedin.png',
-//       iconHover: 'assets/img/header/colleagues_thoughts/linkedin_hover.png',
-//       text: 'COLLEAGUES.CARD_TXT_2',
-//       name: 'Sophia Hartmann',
-//       job: 'Frontend Developer'
-//     },
-//     {
-//       src: 'assets/img/header/colleagues_thoughts/feedback1.png',
-//       srcMobile: 'assets/mobile/mobile_card_3.png',
-//       hoverOverlay: 'assets/img/header/colleagues_thoughts/feedback_hover.png',
-//       icon: 'assets/img/header/colleagues_thoughts/linkedin.png',
-//       iconHover: 'assets/img/header/colleagues_thoughts/linkedin_hover.png',
-//       text: 'COLLEAGUES.CARD_TXT_3',
-//       name: 'Jan Köhler',
-//       job: 'Frontend Developer'
-//     }
-//   ];
-
-//   isMobile = window.innerWidth <= 800;
-//   hoveredIndex: number | null = null;
-
-//   @HostListener('window:resize')
-//   onResize() {
-//     this.isMobile = window.innerWidth <= 800;
-//   }
-
-//   getCardStyle(index: number): { [key: string]: string } {
-//     // if (this.isMobile) {
-
-//     // }
-
-//     switch (index) {
-//       case 0:
-//         return {
-//           top: '30%',
-//           left: '65%',
-//           transform: 'translate(-50%, -50%) rotate(5deg)'
-//         };
-//       case 1:
-//         return {
-//           top: '55%',
-//           left: '50%',
-//           transform: 'translate(-50%, -50%) rotate(0deg)'
-//         };
-//       case 2:
-//         return {
-//           top: '30%',
-//           left: '37%',
-//           transform: 'translate(-50%, -50%) rotate(-5deg)'
-//         };
-//       default:
-//         return {
-//           top: '40%',
-//           left: '20%',
-//           transform: 'translate(-50%, -50%)'
-//         };
-//     }
-//   }
-
-//   // getTextRotation(index: number): string {
-//   //   switch (index) {
-//   //     case 0: return 'rotate(0deg)';
-//   //     case 1: return 'rotate(0deg)';
-//   //     case 2: return 'rotate(0deg)';
-//   //     default: return 'rotate(0deg)';
-//   //   }
-//   // }
-
-//   getTextRotation(index: number): string {
-//   if (this.isMobile) {
-//     return 'rotate(0deg)';
-//   }
-
-//   switch (index) {
-//     case 0:
-//       return 'rotate(5deg)';
-//     case 1:
-//       return 'rotate(0deg)';
-//     case 2:
-//       return 'rotate(-5deg)';
-//     default:
-//       return 'rotate(0deg)';
-//   }
-// }
-
-//   openLinkedIn(card: any) {
-//     if (card.link) { window.open(card.link, '_blank'); }
-//   }
-
-//   baseZ(i: number): number {
-//     return i === 0 ? 30 : i === 1 ? 20 : 10;
-//   }
-
-//   // combinedStyle(i: number): { [key: string]: string | number } {
-//   //   const base = this.getCardStyle(i);
-//   //   return {
-//   //     ...base,
-//   //     position: 'absolute',
-//   //     zIndex: this.hoveredIndex === i ? 999 : this.baseZ(i),
-//   //   };
-//   // }
-
-//   combinedStyle(i: number): { [key: string]: string | number } {
-//     if (this.isMobile) {
-//       return {
-//         position: 'relative',
-//         zIndex: 'auto'
-//       };
-//     }
-
-//     const base = this.getCardStyle(i);
-
-//     return {
-//       ...base,
-//       position: 'absolute',
-//       zIndex: this.hoveredIndex === i ? 999 : this.baseZ(i),
-//     };
-//   }
-// }
-
-
 import { Component, HostListener } from '@angular/core';
 import { AppHeadlineComponent } from '../app-headline/app-headline.component';
 import { CommonModule } from '@angular/common';
@@ -168,6 +16,7 @@ import { TranslateModule } from '@ngx-translate/core';
 export class ColleaguesThoughtsComponent {
   imageCards = [
     {
+      id: 0,
       src: 'assets/img/header/colleagues_thoughts/feedback1.png',
       srcMobile: 'assets/mobile/mobile_card_1.png',
       hoverOverlay: 'assets/img/header/colleagues_thoughts/feedback_hover.png',
@@ -178,16 +27,18 @@ export class ColleaguesThoughtsComponent {
       job: 'Frontend Developer'
     },
     {
+      id: 1,
       src: 'assets/img/header/colleagues_thoughts/feedback2.png',
       srcMobile: 'assets/mobile/mobile_card_2.png',
       hoverOverlay: 'assets/img/header/colleagues_thoughts/feedback_hover.png',
       icon: 'assets/img/header/colleagues_thoughts/linkedin.png',
       iconHover: 'assets/img/header/colleagues_thoughts/linkedin_hover.png',
       text: 'COLLEAGUES.CARD_TXT_2',
-      name: 'Sophia Hartmann',
+      name: 'Sophia Brink',
       job: 'Frontend Developer'
     },
     {
+      id: 2,
       src: 'assets/img/header/colleagues_thoughts/feedback1.png',
       srcMobile: 'assets/mobile/mobile_card_3.png',
       hoverOverlay: 'assets/img/header/colleagues_thoughts/feedback_hover.png',
@@ -208,7 +59,15 @@ export class ColleaguesThoughtsComponent {
   }
 
   private updateScreenSize(): void {
-    this.isMobile = window.innerWidth <= 800;
+    this.isMobile = window.innerWidth <= 900;
+  }
+
+  get displayedCards() {
+    if (this.isMobile) {
+      return [this.imageCards[1], this.imageCards[0], this.imageCards[2]];
+    }
+
+    return this.imageCards;
   }
 
   getCardStyle(index: number): { [key: string]: string } {
@@ -240,18 +99,27 @@ export class ColleaguesThoughtsComponent {
     }
   }
 
-  getTextRotation(index: number): string {
+  getTextRotation(cardId: number): string {
     if (this.isMobile) {
-      return index === 0 ? 'rotate(3deg)' : 'rotate(0deg)';
+      switch (cardId) {
+        case 0:
+          return 'rotate(0deg)';
+        case 1:
+          return 'rotate(-4deg)';
+        case 2:
+          return 'rotate(4deg)';
+        default:
+          return 'rotate(0deg)';
+      }
     }
 
-    switch (index) {
+    switch (cardId) {
       case 0:
-        return 'rotate(5deg)';
+        return 'rotate(0deg)';
       case 1:
         return 'rotate(0deg)';
       case 2:
-        return 'rotate(-5deg)';
+        return 'rotate(0deg)';
       default:
         return 'rotate(0deg)';
     }
@@ -267,14 +135,28 @@ export class ColleaguesThoughtsComponent {
     return i === 0 ? 30 : i === 1 ? 20 : 10;
   }
 
-  combinedStyle(i: number): { [key: string]: string | number } {
+  combinedStyle(i: number, cardId: number): { [key: string]: string | number } {
     if (this.isMobile) {
+      let rotation = 'none';
+
+      switch (cardId) {
+        case 0:
+          rotation = 'rotate(0deg)';
+          break;
+        case 1:
+          rotation = 'rotate(-2deg)';
+          break;
+        case 2:
+          rotation = 'rotate(1deg)';
+          break;
+      }
+
       return {
         position: 'relative',
         zIndex: 'auto',
         top: 'auto',
         left: 'auto',
-        transform: i === 0 ? 'rotate(3deg)' : 'none'
+        transform: rotation
       };
     }
 
