8782f85e4caaf898903c0343ae6ce69cafd53f5d
JoelBaig <131587984+Joe482@users.noreply.github.com>
2026-05-24 11:58:48 +0200

    feat: added back btn mobile on legal notice

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,3 +1,56 @@
+// import { Component, inject, AfterViewInit } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { NavbarComponent } from '../navbar/navbar.component';
+// import { FooterComponent } from '../shared/footer/footer.component';
+// import { TranslateModule } from '@ngx-translate/core';
+// import { Router } from '@angular/router';
+
+// @Component({
+//   selector: 'app-legal-notice',
+//   standalone: true,
+//   imports: [
+//     CommonModule,
+//     NavbarComponent,
+//     FooterComponent,
+//     TranslateModule
+//   ],
+//   templateUrl: './legal-notice.component.html',
+//   styleUrl: './legal-notice.component.scss'
+// })
+// export class LegalNoticeComponent implements AfterViewInit {
+//   isMobileMenuOpen = false;
+//   private router = inject(Router);
+
+//   closeToHomeTop() {
+//     this.router.navigateByUrl('/').then(() => {
+//       requestAnimationFrame(() => {
+//         window.scrollTo({ top: 0, left: 0, behavior: 'instant' as ScrollBehavior });
+//       });
+//     });
+//   }
+
+//   closeToContact() {
+//     this.router.navigateByUrl('/#contact').then(() => {
+//       requestAnimationFrame(() => {
+//         document.getElementById('contact')?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+//       });
+//     });
+//   }
+
+//   ngAfterViewInit(): void {
+//     requestAnimationFrame(() => {
+//       window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+//     });
+//   }
+
+//   onEmailLinkClick(event: MouseEvent) {
+//     event.preventDefault();
+//     this.closeToContact();
+//   }
+// }
+
+
+
 import { Component, inject, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar.component';
@@ -24,7 +77,11 @@ export class LegalNoticeComponent implements AfterViewInit {
   closeToHomeTop() {
     this.router.navigateByUrl('/').then(() => {
       requestAnimationFrame(() => {
-        window.scrollTo({ top: 0, left: 0, behavior: 'instant' as ScrollBehavior });
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'instant' as ScrollBehavior
+        });
       });
     });
   }
@@ -32,14 +89,21 @@ export class LegalNoticeComponent implements AfterViewInit {
   closeToContact() {
     this.router.navigateByUrl('/#contact').then(() => {
       requestAnimationFrame(() => {
-        document.getElementById('contact')?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        document.getElementById('contact')?.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
       });
     });
   }
 
   ngAfterViewInit(): void {
     requestAnimationFrame(() => {
-      window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'auto'
+      });
     });
   }
 
@@ -47,4 +111,4 @@ export class LegalNoticeComponent implements AfterViewInit {
     event.preventDefault();
     this.closeToContact();
   }
-}
+} 
